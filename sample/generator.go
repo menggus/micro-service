acbbe41445d8ef992d34a5fcd6ee46ccd4981c9b
menggus <acupofwater.tao@outlook.com>
2021-07-13 17:54:13 +0800

    update:sample/generator;sample/random; add method [NewKyeboard, NewCPU]

--- sample/generator.go
+++ sample/generator.go
@@ -2,12 +2,36 @@ package sample
 
 import "library/v1/pb"
 
+// NewKeyboard return a sample keyboard
 func NewKeyboard() *pb.Keyboard {
 
 	keyboard := &pb.Keyboard{
-		Layout: randomKeyboardLayout(),
-		Backlit: randomKeyboardBacklist(),
+		Layout:  randomKeyboardLayout(), // generate random keyboard layout
+		Backlit: randBool(),             // generate random  backlit cato
 	}
 	return keyboard
 }
 
+// NewCPU return a sample CPU
+func NewCPU() *pb.CPU {
+	brand := randomCPUBrand()
+	name := randomCPUName(brand)
+
+	numcores := randomInt(2, 8)
+	numthreads := randomInt(4, 32)
+
+	minGhz := randomFloat64(2.0, 5.0)
+	maxGhz := randomFloat64(2.0, 5.0)
+
+	cpu := &pb.CPU{
+		Brand:      brand,
+		Name:       name,
+		NumCores:   uint32(numcores),
+		NumThreads: uint32(numthreads),
+		MinGhz:     minGhz,
+		MaxGhz:     maxGhz,
+	}
+	return cpu
+}
+
+//
--- sample/random.go
+++ sample/random.go
@@ -1,4 +1,61 @@
 package sample
 
+import (
+	"library/v1/pb"
+	"math/rand"
+)
 
+// keyboard
+func randomKeyboardLayout() pb.Keyboard_Layout {
+	switch rand.Intn(3) {
+	case 1:
+		return pb.Keyboard_QWERTY
+	case 2:
+		return pb.Keyboard_QWERTZ
+	default:
+		return pb.Keyboard_AZERTY
+	}
+}
 
+func randBool() bool {
+	// random bool value
+	return rand.Intn(2) == 1
+}
+
+// cpu
+func randomCPUBrand() string {
+	return randomStringFromSet("Intel", "Amd")
+}
+
+func randomCPUName(brand string) string {
+	if brand == "Intel" {
+		return randomStringFromSet("Xeon-2286mM", "Core I9-9980HK", "Core I7-9750H", "Core I5-9400F", "Core I3-1005G1")
+	}
+
+	return randomStringFromSet("Ryzen 7 PRO 2700U", "Ryzen 5 PRO 3500U", "Ryzen 3 3200GE")
+}
+
+// public method random string
+func randomStringFromSet(a ...string) string {
+	n := len(a)
+	if n == 0 {
+		return ""
+	}
+	return a[rand.Intn(n)]
+}
+
+// public method random int
+func randomInt(min int, max int) int {
+	if min > max {
+		min, max = max, min
+	}
+	return min + rand.Intn(max-min)
+}
+
+// public method random float64
+func randomFloat64(min, max float64) float64 {
+	if min > max {
+		min, max = max, min
+	}
+	return min + rand.Float64()*(max-min)
+}
