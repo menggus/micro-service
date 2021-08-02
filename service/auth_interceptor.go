2afa3855d1440df85d2ba9898662f6a53c608f68
menggus <acupofwater.tao@outlook.com>
2021-08-02 17:39:06 +0800

    update server side auth_interceptor

--- service/auth_interceptor.go
+++ service/auth_interceptor.go
@@ -3,6 +3,9 @@ package service
 import (
 	"context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"log"
 )
 
@@ -24,9 +27,14 @@ func (interceptor *AuthInterceptor) Unary() grpc.UnaryServerInterceptor {
 		ctx context.Context,
 		req interface{},
 		info *grpc.UnaryServerInfo,
-		handler grpc.UnaryHandler) (resp interface{}, err error) {
+		handler grpc.UnaryHandler) (interface{}, error) {
 
-		log.Println(">----------------Unary Interceptor: ", info.FullMethod)
+		log.Println(">---------------->Unary Interceptor: ", info.FullMethod)
+
+		err := interceptor.Authorize(ctx, info.FullMethod)
+		if err != nil {
+			return nil, err
+		}
 
 		return handler(ctx, req)
 	}
@@ -41,10 +49,44 @@ func (interceptor *AuthInterceptor) Stream() grpc.StreamServerInterceptor {
 
 		log.Println(">----------------stream Interceptor: ", info.FullMethod)
 
+		err := interceptor.Authorize(ss.Context(), info.FullMethod)
+		if err != nil {
+			return err
+		}
 		return handler(srv, ss)
 	}
 }
 
-func (interceptor *AuthInterceptor) Auth() {
+func (interceptor *AuthInterceptor) Authorize(ctx context.Context, method string) error {
+	accessibleRoles, ok := interceptor.accessibleRoles[method]
+	if !ok {
+		// accessible every one
+		return nil
+	}
 
+	// Get meta data
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return status.Errorf(codes.Unauthenticated, "metadata is not provided")
+	}
+	// Get "authorization" from meta data
+	values := md.Get("authorization")
+	if len(values) == 0 {
+		return status.Errorf(codes.Unauthenticated, "authorization key is not provided")
+	}
+	log.Println("*******************************")
+	// Check the access token valid
+	accessToken := values[0]
+	claims, err := interceptor.jwtManager.Verify(accessToken)
+	if err != nil {
+		return status.Errorf(codes.Unauthenticated, "access token is invalid")
+	}
+
+	for _, role := range accessibleRoles {
+		log.Printf("path: %v ------ user: %v", role, claims.Role)
+		if role == claims.Role {
+			return nil
+		}
+	}
+	return status.Errorf(codes.Unauthenticated, "no permission to access the RPC")
 }
